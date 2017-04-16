dadef22f6754781886f0ec77ce40e9748288ab31
suwigyarathore <Suwi.Rathore@klm.com>
2017-04-16 20:52:43 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,65 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    trigger,
+    state,
+    style,
+    transition,
+    animate,
+    keyframes
+} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+    <div class="row">
+        <div class="columns">
+            <button (click)="toggleMove()">Press me for animation</button>
+        </div>
+        <div class="columns">
+            <div id="content" [@focusPanel]='state' [@movePanel]='state'>Look at me animate</div>
+        </div>
+    </div>
+    `,
+    styles: [`
+        button { font-size:1.8em; }
+        #content { padding:30px; background:#eeeeee; }
+    `],
+    animations: [
+
+        trigger('focusPanel', [
+            state('inactive', style({
+                transform: 'scale(1)',
+                backgroundColor: '#eee'
+            })),
+            state('active', style({
+                transform: 'scale(1.1)',
+                backgroundColor: '#cfd8dc'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-out'))
+        ]),
+
+        trigger('movePanel', [
+
+            transition('void => *', [
+                animate(600, keyframes([
+                    style({opacity: 0, transform: 'translateY(-200px)', offset: 0}),
+                    style({opacity: 1, transform: 'translateY(100px)', offset: .75}),
+                    style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+                ]))
+            ])
+
+        ])
+
+
+    ]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+
+    state: string = 'inactive';
+
+    toggleMove() {
+        this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+    }
+
+}
